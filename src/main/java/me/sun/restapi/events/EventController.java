d37bc14886c5902f96859fc82f74898502803435
lee <dm951125@Naver.com>
2019-11-11 03:12:18 +0900

    Model Mapper 이용하여 Dto 변환, 입력값 제한하기 테스트 추가

--- src/main/java/me/sun/restapi/events/EventController.java
+++ src/main/java/me/sun/restapi/events/EventController.java
@@ -1,10 +1,10 @@
 package me.sun.restapi.events;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +20,12 @@ public class EventController {
 
     private final EventRepository eventRepository;
 
+    //모델 매퍼를 사용하면 Dto값을 편리하게 변환 가능
+    private final ModelMapper modelMapper;
+
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event savedEvent = this.eventRepository.save(event);
         URI uri = linkTo(EventController.class).slash(savedEvent.getId()).toUri();
         return ResponseEntity.created(uri).body(event);
