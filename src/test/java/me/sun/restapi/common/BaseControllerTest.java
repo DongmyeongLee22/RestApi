7af48bfe235123956b88e90a28d2b44aa18ee138
lee <dm951125@Naver.com>
2019-11-12 15:26:40 +0900

    스프링 시큐리티 OAuth2 인증서버 설정

--- src/test/java/me/sun/restapi/common/BaseControllerTest.java
+++ src/test/java/me/sun/restapi/common/BaseControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.ui.ModelMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
