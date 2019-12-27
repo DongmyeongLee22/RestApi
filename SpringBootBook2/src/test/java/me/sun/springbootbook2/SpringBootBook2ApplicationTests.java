b059c2e66c354a1ddbbabf58ba5fb0651f690dc3
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-28 01:36:33 +0900

    initial commit

---
+++ SpringBootBook2/src/test/java/me/sun/springbootbook2/SpringBootBook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sun.springbootbook2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBook2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
